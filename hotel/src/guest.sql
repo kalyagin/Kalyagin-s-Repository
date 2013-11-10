156a1cc4deae791af4ba548aad8d8fe7493fd0c7
Kalyagin Ilya <kalyagin.ilya@gmail.com>
2013-11-10 19:14:10 +0400

    продолжение

---
+++ hotel/src/guest.sql
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author михаил
+ * @name guest
+ * @manual 
+ */ 
+Select * 
+From GUESTS t1
+ Where :guestView = t1.GUESTS_ID
---
+++ hotel/src/mainGRhotel.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @author михаил
+ * @name mainGRhotel
+ */
+
+function buttonActionPerformed(evt) {//GEN-FIRST:event_buttonActionPerformed
+	
+}//GEN-LAST:event_buttonActionPerformed
